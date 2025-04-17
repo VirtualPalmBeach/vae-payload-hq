4d5a0fa494062eb5df593e74ed32a4d70a57a34b
VirtualPalmBeach <116676155+VirtualPalmBeach@users.noreply.github.com>
2025-04-17 18:15:21 -0500

    Update homepage.ts

--- src/collections/homepage.ts
+++ src/collections/homepage.ts
@@ -6,27 +6,20 @@ export const Homepage = {
   fields: [
     {
       name: 'title',
-      label: 'Title',
       type: 'text',
       required: true,
     },
     {
       name: 'introText',
-      label: 'Intro Text',
       type: 'textarea',
-      required: false,
     },
     {
       name: 'ctaText',
-      label: 'Call to Action Text',
       type: 'text',
-      required: false,
     },
     {
       name: 'ctaLink',
-      label: 'Call to Action Link',
       type: 'text',
-      required: false,
     }
   ],
 };
