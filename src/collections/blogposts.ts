89e92b8daa6c7b436d52b34bd70fa09ce86ea25b
VirtualPalmBeach <116676155+VirtualPalmBeach@users.noreply.github.com>
2025-04-18 16:34:48 +0000

    Update: blogposts.ts for import type path

--- src/collections/blogposts.ts
+++ src/collections/blogposts.ts
@@ -1,4 +1,4 @@
-import { CollectionConfig } from 'payload/types';
+import { CollectionConfig } from 'payload';
 import { commonSiteKeyField } from './commonSiteKeyField';
 
 export const BlogPosts: CollectionConfig = {
