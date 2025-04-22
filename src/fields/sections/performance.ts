c97849c5aaf6217ed0baf61095e2b0028b9c686c
VirtualPalmBeach <116676155+VirtualPalmBeach@users.noreply.github.com>
2025-04-22 18:55:32 +0000

    typo: performanceFields

--- src/fields/sections/performance.ts
+++ src/fields/sections/performance.ts
@@ -1,7 +1,7 @@
 import { Field } from 'payload/types';
 import { validateUrl } from '../shared/validate';
 
-const analyticsFields = [
+const performanceFields = [
   {
     name: 'performance',
     type: 'group',
