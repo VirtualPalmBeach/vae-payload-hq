418ca4b8208b779e0a01eae0a990ffac36ebb15d
VirtualPalmBeach <116676155+VirtualPalmBeach@users.noreply.github.com>
2025-04-23 02:17:00 +0000

    Update: Inlined siteKey Field siteSettings.ts

--- src/collections/siteSettings.ts
+++ src/collections/siteSettings.ts
@@ -25,6 +25,21 @@ const SiteSettings: CollectionConfig = {
       type: 'checkbox',
     },
     {
+      name: 'siteKey',
+      label: 'Site Key',
+      type: 'select',
+      options: [
+        { label: 'Selah Pools', value: 'selahPools' },
+        { label: 'Selah Pro', value: 'selahPro' },
+        { label: 'DFW Pool Builder', value: 'dfwPoolBuilder' },
+        { label: 'Southlake Outdoor', value: 'southlakeOutdoor' },
+        { label: 'Omega Pool Services', value: 'omegaPoolServices' },
+      ],
+      defaultValue: 'selahPro',
+      required: true,
+      admin: { position: 'sidebar' },
+    },
+    {
       type: 'tabs',
       tabs: [
         {
